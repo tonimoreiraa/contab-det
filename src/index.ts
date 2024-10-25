76ef2dd8a6a2a66311d2fcedaed1d596d1a8d3a1
Toni Moreira <moreiraatoni@gmail.com>
2024-10-25 18:08:07 -0300

    Refactor code to extract company running logic into a separate function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,150 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Page } from "puppeteer";
 import { logger } from "./logger";
 import { getCompanies } from "./input";
 import { cnpjFormatter } from "./cnpj";
+
+async function runCompany(page: Page, company: any, i: number) {
+  if (i != 0) {
+    await page.goto("https://det.sit.trabalho.gov.br/servicos");
+  }
+
+  const name = Object.values(company)[0];
+  const changeProfileButton = await page.waitForSelector(
+    "#content app-info-empregador .is-secondary"
+  );
+  await new Promise((r) => setTimeout(r, 1000));
+  changeProfileButton?.click();
+
+  await page.waitForSelector("ng-select");
+  await page.evaluate(() => {
+    var clickEvent = new Event("mousedown", {
+      bubbles: true,
+      cancelable: true,
+    });
+    document.querySelector("ng-select > div").dispatchEvent(clickEvent);
+  });
+  const procuradorButton = await page.waitForSelector(
+    `.ng-option:last-child`
+  );
+  await procuradorButton?.click();
+
+  const cnpj = cnpjFormatter(company.CNPJ);
+  logger.info(`Logando na empresa ${name} com CNPJ: ${cnpj}`);
+  await page.$eval(
+    "body > modal-container > div > div > app-modal-perfil > div.modal-body > form > div:nth-child(2) > div > br-input > div > div > input",
+    (input, value) => {
+      input.value = value;
+      const event = new Event("input", { bubbles: true });
+      input.dispatchEvent(event);
+    },
+    cnpj
+  );
+
+  await page.click(
+    "body > modal-container > div > div > app-modal-perfil > div.modal-body > form > div.row.barra-botoes > div > button.br-button.is-primary"
+  );
+  await page.waitForFunction(() => {
+    return (
+      !document.body.classList.contains("modal-open") ||
+      !!document.querySelector(".br-message.is-danger") ||
+      document.querySelector(".br-input.is-invalid")
+    );
+  });
+  if (
+    !(
+      (await page.$(".br-message.is-danger")) ||
+      (await page.$(".br-input.is-invalid"))
+    )
+  ) {
+    logger.info(`Logado na empresa ${company["EMPRESA"]}: ${cnpj}`);
+
+    await page.goto("https://det.sit.trabalho.gov.br/cadastro", {
+      waitUntil: "networkidle0",
+    });
+
+    const hasContactRegistered = !(await page.$(
+      "#content > app-cadastro > br-title > br-alert-messages > aside > div.br-message.is-warning"
+    ));
+    logger.info(
+      `Tem contatos registrados: ${hasContactRegistered ? "Sim" : "Não"}`
+    );
+
+    if (!hasContactRegistered) {
+      logger.info("Registrando palavra chave.");
+
+      const keyworkInput = await page.waitForSelector(
+        "#content > app-cadastro > form > div:nth-child(1) > div > br-input > div > div > input"
+      );
+      await keyworkInput?.type("CLEOSELMA");
+
+      await page.click(
+        "#content > app-cadastro > form > div.br-table > div > span > a"
+      );
+
+      logger.info("Registrando contato.");
+      const nameInput = await page.waitForSelector(
+        "body > modal-container > div > div > app-contato-cadastro > div.modal-body > form > div > div:nth-child(1) > br-input > div > div > input"
+      );
+      const emailInput = await page.$(
+        "body > modal-container > div > div > app-contato-cadastro > div.modal-body > form > div > div:nth-child(2) > br-input > div > div > input"
+      );
+      const phoneInput = await page.$(
+        "body > modal-container > div > div > app-contato-cadastro > div.modal-body > form > div > div:nth-child(3) > br-input > div > div > input"
+      );
+
+      await nameInput?.type("CLEOSELMA");
+      await emailInput?.type("pessoal@cleodoncontabilidade.com.br");
+      await phoneInput?.type("(82) 99626-4298");
+      logger.info("Salvando contato.");
+      await page.click(
+        "body > modal-container > div > div > app-contato-cadastro > div.modal-footer > button.br-button.is-primary"
+      );
+      await page.waitForFunction(
+        () => !document.body.classList.contains("modal-open")
+      );
+      logger.info("Salvando cadastro.");
+      await new Promise((r) => setTimeout(r, 500));
+      await page.click(
+        "#content > app-cadastro > form > div.row.barra-botoes > div > button"
+      );
+      await page.waitForSelector(
+        "#content > app-cadastro > br-title > br-alert-messages > aside > div"
+      );
+      logger.info("Cadastro finalizado");
+    }
+
+    logger.info("Salvando print da caixa postal");
+    await page.goto("https://det.sit.trabalho.gov.br/caixapostal");
+    await page.waitForSelector(".tabela_mensagens");
+    await page.waitForFunction(
+      () => {
+        const element = document.querySelector(".br-load.is-loading");
+        return !element;
+      },
+      { timeout: 30000 }
+    );
+    await page.screenshot({
+      path: `output/${name}-caixa-postal.png`,
+    });
+
+    logger.info("Salvando print das notificações");
+    await page.goto("https://det.sit.trabalho.gov.br/notificacao");
+    await page.waitForSelector("br-title");
+    await page.waitForFunction(
+      () => {
+        const element = document.querySelector(".br-load.is-loading");
+        return !element;
+      },
+      { timeout: 30000 }
+    );
+    await page.screenshot({
+      path: `output/${name}-notificacoes.png`,
+    });
+    logger.info(`Empresa ${name} finalizada`);
+  } else {
+    logger.error(`Login falhou: Empresa ${name} com CNPJ: ${cnpj}`);
+  }
+}
 
 declare var document: any;
 
@@ -30,149 +173,14 @@ async function main() {
 
   while (i < companies.length) {
     try {
-      if (i != 0) {
-        await page.goto("https://det.sit.trabalho.gov.br/servicos");
-      }
-
-      const company = companies[i];
-      const name = Object.values(company)[0];
-      const changeProfileButton = await page.waitForSelector(
-        "#content app-info-empregador .is-secondary"
-      );
-      await new Promise((r) => setTimeout(r, 1000));
-      changeProfileButton?.click();
-
-      await page.waitForSelector("ng-select");
-      await page.evaluate(() => {
-        var clickEvent = new Event("mousedown", {
-          bubbles: true,
-          cancelable: true,
-        });
-        document.querySelector("ng-select > div").dispatchEvent(clickEvent);
-      });
-      const procuradorButton = await page.waitForSelector(
-        `.ng-option:last-child`
-      );
-      await procuradorButton?.click();
-
-      const cnpj = cnpjFormatter(company.CNPJ);
-      logger.info(`Logando na empresa ${name} com CNPJ: ${cnpj}`);
-      await page.$eval(
-        "body > modal-container > div > div > app-modal-perfil > div.modal-body > form > div:nth-child(2) > div > br-input > div > div > input",
-        (input, value) => {
-          input.value = value;
-          const event = new Event("input", { bubbles: true });
-          input.dispatchEvent(event);
-        },
-        cnpj
-      );
-
-      await page.click(
-        "body > modal-container > div > div > app-modal-perfil > div.modal-body > form > div.row.barra-botoes > div > button.br-button.is-primary"
-      );
-      await page.waitForFunction(() => {
-        return (
-          !document.body.classList.contains("modal-open") ||
-          !!document.querySelector(".br-message.is-danger") ||
-          document.querySelector(".br-input.is-invalid")
-        );
-      });
-      if (
-        !(
-          (await page.$(".br-message.is-danger")) ||
-          (await page.$(".br-input.is-invalid"))
-        )
-      ) {
-        logger.info(`Logado na empresa ${company["EMPRESA"]}: ${cnpj}`);
-
-        await page.goto("https://det.sit.trabalho.gov.br/cadastro", {
-          waitUntil: "networkidle0",
-        });
-
-        const hasContactRegistered = !(await page.$(
-          "#content > app-cadastro > br-title > br-alert-messages > aside > div.br-message.is-warning"
-        ));
-        logger.info(
-          `Tem contatos registrados: ${hasContactRegistered ? "Sim" : "Não"}`
-        );
-
-        if (!hasContactRegistered) {
-          logger.info("Registrando palavra chave.");
-
-          const keyworkInput = await page.waitForSelector(
-            "#content > app-cadastro > form > div:nth-child(1) > div > br-input > div > div > input"
-          );
-          await keyworkInput?.type("CLEOSELMA");
-
-          await page.click(
-            "#content > app-cadastro > form > div.br-table > div > span > a"
-          );
-
-          logger.info("Registrando contato.");
-          const nameInput = await page.waitForSelector(
-            "body > modal-container > div > div > app-contato-cadastro > div.modal-body > form > div > div:nth-child(1) > br-input > div > div > input"
-          );
-          const emailInput = await page.$(
-            "body > modal-container > div > div > app-contato-cadastro > div.modal-body > form > div > div:nth-child(2) > br-input > div > div > input"
-          );
-          const phoneInput = await page.$(
-            "body > modal-container > div > div > app-contato-cadastro > div.modal-body > form > div > div:nth-child(3) > br-input > div > div > input"
-          );
-
-          await nameInput?.type("CLEOSELMA");
-          await emailInput?.type("pessoal@cleodoncontabilidade.com.br");
-          await phoneInput?.type("(82) 99626-4298");
-          logger.info("Salvando contato.");
-          await page.click(
-            "body > modal-container > div > div > app-contato-cadastro > div.modal-footer > button.br-button.is-primary"
-          );
-          await page.waitForFunction(
-            () => !document.body.classList.contains("modal-open")
-          );
-          logger.info("Salvando cadastro.");
-          await new Promise((r) => setTimeout(r, 500));
-          await page.click(
-            "#content > app-cadastro > form > div.row.barra-botoes > div > button"
-          );
-          await page.waitForSelector(
-            "#content > app-cadastro > br-title > br-alert-messages > aside > div"
-          );
-          logger.info("Cadastro finalizado");
-        }
-
-        logger.info("Salvando print da caixa postal");
-        await page.goto("https://det.sit.trabalho.gov.br/caixapostal");
-        await page.waitForSelector(".tabela_mensagens");
-        await page.waitForFunction(
-          () => {
-            const element = document.querySelector(".br-load.is-loading");
-            return !element;
-          },
-          { timeout: 30000 }
-        );
-        await page.screenshot({
-          path: `output/${name}-caixa-postal.png`,
-        });
-
-        logger.info("Salvando print das notificações");
-        await page.goto("https://det.sit.trabalho.gov.br/notificacao");
-        await page.waitForSelector("br-title");
-        await page.waitForFunction(
-          () => {
-            const element = document.querySelector(".br-load.is-loading");
-            return !element;
-          },
-          { timeout: 30000 }
-        );
-        await page.screenshot({
-          path: `output/${name}-notificacoes.png`,
-        });
-        logger.info(`Empresa ${name} finalizada`);
-      } else {
-        logger.error(`Login falhou: Empresa ${name} com CNPJ: ${cnpj}`);
-      }
-
-       i++
+      const company = companies[i]
+      await runCompany(page, company, i)
+        .catch(_ => {
+          i--
+        })
+        .then(() => {
+          i++
+        })
     } catch (error) {
       retryCount++
 
